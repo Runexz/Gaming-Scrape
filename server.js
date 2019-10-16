66776383465e848d83354c4a4d69193084112747
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-16 11:28:49 -0500

    added npm require and var port

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// server.js
+// Variable used to grab from NPM dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+
+// Port to be either the host's port or 3000
+var PORT = process.env.PORT || 3000;
+
+// Initialize NPM Express
+var app = express();
