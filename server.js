6cb65b722b0711d4d23ce73fc4d846f0791c7720
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-16 11:34:25 -0500

    handlebar access and mongoose connection

--- server.js
+++ server.js
@@ -8,3 +8,31 @@ var PORT = process.env.PORT || 3000;
 
 // Initialize NPM Express
 var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// If app is deployed, use the deployed database. Otherwise use the local database called gamingHeadlines
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gameingHeadlines";
+
+// Connect to the mongo DB
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+    console.log(`db error ${err.message}`);
+    });
+
+// Listen on port
+app.listen(PORT, function () {
+    console.log("Listening on port:" + PORT);
+})
