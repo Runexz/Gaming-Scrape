3795e4ddfe343229a6a1be897c33730d28bce3f3
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-16 13:25:12 -0500

    added router variable

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ var PORT = process.env.PORT || 3000;
 // Initialize NPM Express
 var app = express();
 
+// Sets up Express router
+var router = express.Router();
+
+// Require routes file
+require("./config/routes")(router);
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, "/public")));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -19,18 +26,21 @@ app.use(express.json());
 app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// If app is deployed, use the deployed database. Otherwise use the local database called gamingHeadlines
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gameingHeadlines";
+// All requests go through the router middleware
+app.use(router);
 
-// Connect to the mongo DB
-mongoose.connect(MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    })
-    .then(() => console.log('DB Connected!'))
-    .catch(err => {
-    console.log(`db error ${err.message}`);
-    });
+// // If app is deployed, use the deployed database. Otherwise use the local database called gamingHeadlines
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gameingHeadlines";
+
+// // Connect to the mongo DB
+// mongoose.connect(MONGODB_URI, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     })
+//     .then(() => console.log('DB Connected!'))
+//     .catch(err => {
+//     console.log(`db error ${err.message}`);
+//     });
 
 // Listen on port
 app.listen(PORT, function () {
