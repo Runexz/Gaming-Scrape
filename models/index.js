45902572784bd9d8c2444da70be8d50ff03b1677
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-17 18:29:12 -0500

    a=A n=N changed

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    article: require("./Article"),
-    note: require("./Note")
+    Article: require("./Article"),
+    Note: require("./Note")
   };
