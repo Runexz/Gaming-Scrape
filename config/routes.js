1e652931860223d52c06f787d63a0c1346d9ed03
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-18 02:50:46 -0500

    require Article and Note from models

--- config/routes.js
+++ config/routes.js
@@ -1,18 +1,28 @@
+// Require all models
+var Article = require("../models/Article");
+var Note = require("../models/Note");
+
 // Include routes in server file
 module.exports = function (router) {
     // Route renders the home handlebars page
     router.get("/", function (req, res) {
-        res.render("articles", {
-            style: "style.css",
-            javascript: "articles.js"
-        });
+        Article.find(function (err, Article) {
+            res.render("articles", {
+                style: "style.css",
+                javascript: "articles.js",
+                articleSchema: Article
+            });
+        })
     });
 
     // Route renders the saved handlebars page
     router.get("/saved", function (req, res) {
-        res.render("saved", {
-            style: "saved.css",
-            javascript: "saved.js"
-        });
+        Note.find(function (err, Note) {
+            res.render("saved", {
+                style: "saved.css",
+                javascript: "saved.js",
+                noteSchema: Note
+            });
+        })
     });
 }
