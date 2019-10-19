020c5f5a930de2080265e2b0a843eb9b31500f60
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-19 17:23:38 -0500

    format document

--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -1,37 +1,6 @@
 
-// When the Make a note button is pressed save the article
-// $(document).on("click", ".card", function() {
-//     console.log("This article has been selected: ")
-//     var thisId = $(this).children("div").children("h3").attr("data-id");
-    
-//     console.log(thisId);
-
-    // $('#exampleModal').modal('toggle')
-    
-    // $(".save-note").click(function(){
-    //     event.preventDefault();
-    //     console.log("I was pressed");
-    //     console.log($(".form-control").text());
-    // })
-
-    // $.ajax({
-    //   method: "POST",
-    //   url: "/notes",
-    //   data: {
-    //     title: thisTitle,
-    //     summary: thisSummary,
-    //     link: thisLink
-    //   }
-    // })
-    // .then(function(data) {
-    //   console.log(data);
-      
-    // })
-    
-
-// })
-
-$(document).on("click", ".card", function(){
+// Gets the ID number, text of the note and updates the Note Database
+$(document).on("click", ".card", function () {
     event.preventDefault();
 
     var thisID = $(this).children("div").children("h3").attr("data-id");
@@ -47,13 +16,11 @@ $(document).on("click", ".card", function(){
             note: newNote
         }
     })
-    // With that done
-    .then(function(data) {
-        // Log the response
-        console.log(data);
-        
-        
-      });
-      location.reload();
-
-})
+        // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+        });
+    // Reload the page
+    location.reload();
+});
