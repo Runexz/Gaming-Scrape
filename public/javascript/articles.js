e6d7cb87838637582701ae6a0507c2778c6f5290
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-19 13:29:32 -0500

    added Post

--- public/javascript/articles.js
+++ public/javascript/articles.js
@@ -1,49 +1,49 @@
 
 // When the SCRAPE NEW ARTICLES button is pressed ajax will be used to scrape data and reload the page.
-$( ".scrape-new" ).click(function() {
-    console.log("SCAPED button has been pushed!")
+$(".scrape-new").click(function () {
+  console.log("SCAPED button has been pushed!")
 
-    // Ajax will use the GET method to URL: /scrape that is pulled from server.js
-    $.ajax({
-        method: "GET",
-        url: "/scrape"
-      })
-      .then(function(data) {
-          console.log(data)
-      })
-    alert( "The articles have been gathered" );
-
-    // Reload the page so handlebars can display those artiles
-    location.reload();
-    console.log("The page has been reloaded!")
-  });
-
-  // When the Make a note button is pressed save the article
-  $(document).on("click", ".card", function() {
-      console.log("This article has been selected: ")
-      var thisId = $(this).children("div").children("h3").attr("data-id");
-      var thisTitle = $(this).children("div").children("h5").text();
-      var thisSummary = $(this).children("div").children("p").text();
-      var thisLink = $(this).children("div").children("a").attr("href");
-      console.log(thisId);
-      console.log(thisTitle)
-      console.log(thisSummary)
-      console.log(thisLink)
-
-      $.ajax({
-        method: "POST",
-        url: "/notes",
-        data: {
-          title: thisTitle,
-          summary: thisSummary,
-          link: thisLink
-        }
-      })
-      .then(function(data) {
-        console.log(data);
-        $('#exampleModal').modal('toggle')
-      })
-      
-    //   alert("Your Article has been saved")
-
+  // Ajax will use the GET method to URL: /scrape that is pulled from server.js
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
   })
+    .then(function (data) {
+      console.log(data)
+    })
+  alert("The articles have been gathered");
+
+  // Reload the page so handlebars can display those artiles
+  location.reload();
+  console.log("The page has been reloaded!")
+});
+
+// When the Make a note button is pressed save the article
+$(document).on("click", ".card", function () {
+  console.log("This article has been selected: ")
+  var thisId = $(this).children("div").children("h3").attr("data-id");
+  var thisTitle = $(this).children("div").children("h5").text();
+  var thisSummary = $(this).children("div").children("p").text();
+  var thisLink = $(this).children("div").children("a").attr("href");
+  console.log(thisId);
+  console.log(thisTitle)
+  console.log(thisSummary)
+  console.log(thisLink)
+
+  $.ajax({
+    method: "POST",
+    url: "/notes",
+    data: {
+      title: thisTitle,
+      summary: thisSummary,
+      link: thisLink
+    }
+  })
+    .then(function (data) {
+      console.log(data);
+      $('#exampleModal').modal('toggle')
+    })
+
+  //   alert("Your Article has been saved")
+
+})
