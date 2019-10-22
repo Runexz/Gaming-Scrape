c052000fa241755d82e91c3037d92722099a6134
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-10-22 18:00:13 -0500

    variable added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
+var Article = require('./Article');
+var Note = require('./Note');
+
 // Exporting an object containing all of our models
 module.exports = {
-  Article: require("./Article"),
-  Note: require("./Note")
+  Article: Article,
+  Note: Note
 };
